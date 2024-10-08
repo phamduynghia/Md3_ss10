7beeae580db966d5767c7a82e5bbd84f93911f42
phamduynghia <dr3amm1n1st3r@gmail.com>
2024-10-08 22:46:00 +0700

    nup bai

---
+++ Md3_ss10/src/main/java/com/ra/md3_ss10/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.ra.md3_ss10;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
